b85cded7980ed0cb5d6d1cc6f812c9efd7914da4
rs222kn <rs222kn@student.lnu.se>
2017-03-13 22:26:40 +0100

    updated with config option and added comments

--- app.js
+++ app.js
@@ -3,32 +3,73 @@
 const crypto = require('crypto')
 const bufferEq = require('buffer-equal-constant-time')
 
+const xHubSignatureNotFound = 'xHubSignatureNotFound'
+const verifyingSignatureFailed = 'verifyingSignatureFailed'
+
+const defaultConfig = {
+  [xHubSignatureNotFound]: {
+    status: 400,
+    errorMessage: 'No signature found in headers',
+  },
+  [verifyingSignatureFailed]: {
+    status: 403,
+    errorMessage: 'Verifying signature failed',
+  },
+}
+
 const sign = (secret, data) => 
   'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex')
 
+/**
+ * Compares in equal time
+ * @param {String} secret    [secret to compare sign with]
+ * @param {String} data      []
+ * @param {String} signature [compare secret with]
+ * @returns {Boolean} [is same]
+ */
 const verifySignature = (secret, data, signature) => 
   bufferEq(new Buffer(signature), new Buffer(sign(secret, data)))
 
-const githubWebhook = (secret) => (req, res, next) => {
+/**
+ * Verify github secret on request
+ * @param {String} secret [string to compare with]
+ * @param {Object} config [object with default props]
+ */
+const verifySecret = (secret, config = defaultConfig) => {
   if(typeof secret !== 'string'){
     throw new TypeError(`Secret must be string! Secret type is: ${typeof secret}`)
   }
 
-  const sign = req.headers['x-hub-signature']
-  if(!sign){
-    return res.status(400).send({
-      error: 'No signature found in headers',
-    })
-  }
+  const { 
+    [xHubSignatureNotFound]: { 
+      status: xHubStatus, 
+      errorMessage: xHubErrorMessage,
+    },
+    [verifyingSignatureFailed]: {
+      status: signatureStatus,
+      errorMessage: signatureErrorMessage,
+    },
+  } = config
 
-  if(!verifySignature(secret, JSON.stringify(req.body), sign)){
-    return res.status(403).send({
-      error: 'verifying signature failed',
-    })
-  }
+  /**
+   * Express middleware function
+   */
+  return (req, res, next) => {
+    const sign = req.headers['x-hub-signature']
+    if(!sign){
+      return res.status(xHubStatus).send({
+        error: xHubErrorMessage,
+      })
+    }
 
-  next()
+    if(!verifySignature(secret, JSON.stringify(req.body), sign)){
+      return res.status(signatureStatus).send({
+        error: signatureErrorMessage,
+      })
+    }
+
+    next()
+  }
 }
 
-module.exports = githubWebhook
-
+module.exports = verifySecret
